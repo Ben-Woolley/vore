96ccedb467b182c57e44247de28f1e73e0cbd5a7
Benjamin Rosenthal <ben.s.woolley@gmail.com>
2025-10-23 20:06:30 +0100

    update build for push to local registry

--- build.sh
+++ build.sh
@@ -5,7 +5,4 @@ set -e
 go mod vendor
 
 # Build image (uses vendor modules)
-docker build . -t ben-woolley/vore:latest
-
-# Save image as tar file for import onto server
-docker save -o ./vore.tar ben-woolley/vore:latest
+docker build . -t localhost:5000/ben-woolley/vore:latest
